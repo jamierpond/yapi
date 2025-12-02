00d9cd4fd45feb262af4282101fd1fd404e02bc9
Jamie Pond <jamiepond259@gmail.com>
2025-12-02 00:20:37 -0800

    wip

--- internal/utils/fn.go
+++ internal/utils/fn.go
@@ -9,17 +9,6 @@ func Map[T, U any](ts []T, f func(T) U) []U {
 	return us
 }
 
-// Filter returns a new slice containing only elements where f(t) is true.
-func Filter[T any](ts []T, f func(T) bool) []T {
-	var matches []T
-	for _, t := range ts {
-		if f(t) {
-			matches = append(matches, t)
-		}
-	}
-	return matches
-}
-
 // ContainsFunc returns true if any element satisfies f.
 func ContainsFunc[T any](ts []T, f func(T) bool) bool {
 	for _, t := range ts {
