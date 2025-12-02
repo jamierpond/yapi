940102f3372196c8f5d363eaab407ebd4bd2e877
Jamie Pond <jamiepond259@gmail.com>
2025-12-02 00:19:16 -0800

    wip

--- internal/utils/fn_test.go
+++ internal/utils/fn_test.go
@@ -1,14 +1,5 @@
 package utils
 
-// Map transforms a slice of T to a slice of U.
-func Map[T, U any](ts []T, f func(T) U) []U {
-	us := make([]U, len(ts))
-	for i, t := range ts {
-		us[i] = f(t)
-	}
-	return us
-}
-
 // Filter returns a new slice containing only elements where f(t) is true.
 func Filter[T any](ts []T, f func(T) bool) []T {
 	var matches []T
